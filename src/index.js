98dcf142583d21b3d3336d77fb5e84e5cc9412b1
Danyil-Repenko <danyil.oficial@gmail.com>
2022-11-26 01:42:23 +0200

    basename

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <BrowserRouter>
+      <BrowserRouter basename='/goit-react-hw-08-phonebook/'>
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
             <HelmetProvider>
